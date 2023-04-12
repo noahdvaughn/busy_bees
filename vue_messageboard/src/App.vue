4e167245804566a33d250e8e887a141c903a0759
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-11 23:07:22 -0400

    adding google oauth

--- vue_messageboard/src/App.vue
+++ vue_messageboard/src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div v-if="state.username === '' || state.username === null">
-    <form @submit.prevent="Login">
+<header>
+  <div v-if="state.username === '' || state.username === null"></div>
+  <div v-else>
+    <h1>Welcome, {{ state.username }}</h1>
+      <button class="logout" @click="Logout">Logout</button>
+  </div>
+
+</header>
+
+  <div v-if="state.username === '' || state.username === null" class="view login">
+    <form @submit.prevent="Login" class="loginForm">
       <h1>Pick your username</h1>
       <label for="username"></label>
       <input type="text" v-model="inputUsername" placeholder="enter username">
       <input type="submit" value="Login">
     </form>
+
+    <h1>Is initialized: {{ Vue3GoogleOauth.isInit }}</h1>
   </div>
 
-  <div v-else>
-    <header>
-      <button class="logout" @click="Logout">Logout</button>
-      <h1>Welcome, {{ state.username }}</h1>
-    </header>
+  <div v-else class="view">
     <section class="chat-box">
-
       <div v-for="message in state.messages" :key="message.key" :class="(message.username == state.username ? 'message current' : 'message')">
       <div class="message-inner"> {{ message.content }}</div>
-      <div class="username">{{ message.username }}</div>
+      <div class="username"> - {{ message.username }}</div>
       </div>
     </section>
 
@@ -32,10 +38,18 @@
 
 <script>
 import db from './db'
-import {reactive, onMounted, ref} from 'vue'
+import {reactive, onMounted, ref, inject} from 'vue'
 
 export default {
   setup () {
+    const Vue3GoogleOauth = inject('Vue3GoogleOauth');
+    
+
+
+
+
+
+
     const inputUsername = ref("")
     const inputMessage = ref("")
 
@@ -92,12 +106,35 @@ export default {
       state,
       inputMessage,
       SendMessage,
-      Logout
+      Logout,
+      Vue3GoogleOauth
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+body {
+ margin: 0;
+}
+header{
+  background-color: grey;
+  height: 10vh;
+}
+.loginForm{
+  text-align: center;
+}
+.view{
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  
+  &.login{
+   }
+}
+
 
 </style>
--- vue_messageboard/src/main.js
+++ vue_messageboard/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+import gAuthPlugin from 'vue3-google-oauth2'
+let gauthClientId =
+  '542822140028-hi9ml494bongv164bkcig8jpu0vlo7kq.apps.googleusercontent.com'
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: 'email',
+  prompt: 'consent'
+})
+app.mount('#app')
