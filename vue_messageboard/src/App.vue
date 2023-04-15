d6ba73c9b55103cf56beb097c4124484182d86fd
Noah Vaughn <noahdvaughn@gmail.com>
2023-04-15 13:20:57 -0400

    finished styling div

--- vue_messageboard/src/App.vue
+++ vue_messageboard/src/App.vue
@@ -1,39 +1,59 @@
 <template>
 <header>
   <div v-if=" state.currentUser === null || state.currentUser.email === undefined ">
-  <h1 class="headerText">DoTheThing</h1>
+  <h1 class="headerText">Busy Bees</h1>
   </div>
-  <div v-else>
+  <div v-else class="loginHeader"> 
     <h1>Welcome {{ state.currentUser.given_name }} {{ state.currentUser.family_name }}</h1>
-      <button class="logout" @click="Logout">Logout</button>
     <!-- <googleLogout :callback="callback"/> -->
-
+    
   </div>
-
+  
 </header>
 
-  <div v-if="state.currentUser === null ||state.currentUser.email === undefined  " class="view">
-    <div class="login">
-      <h3 class="instruction">Sign in, you busy bee</h3>
-      <GoogleLogin :callback="callback"/>
-    </div>
+<div v-if="state.currentUser === null ||state.currentUser.email === undefined  " class="view">
+  <div class="login">
+    <h3 class="instruction">Sign in, you busy bee</h3>
+    <GoogleLogin :callback="callback"/>
+  </div>
+</div>
+
+<div v-else class="view">
+  
+  <div class="buttons">
+    <button class="button" @click="Logout">Logout</button>
+    <button class="button" @click="ToggleViewing">View Completed Tasks</button>
   </div>
 
-  <div v-else class="view">
-
-    <section class="chat-box">
+    <section v-if="state.viewingCompleted === false" class="chat-box">
       <div v-for="message in state.messages" :key="message.key">
-        <div v-if="message.email === state.currentUser.email" class="taskRow">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/1024px-Eo_circle_green_checkmark.svg.png?20200417132424" class="check"/>
+        <div v-if="message.email === state.currentUser.email && message.completed === 'false'" class="taskRow">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/1024px-Eo_circle_green_checkmark.svg.png?20200417132424" class="check" @click="()=>{
+            markCompleted(message)
+          }"/>
           
           <div class="task">
             <p class="content"> {{ message.content }}</p>
           </div>
           
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Eo_circle_red_letter-x.svg/2048px-Eo_circle_red_letter-x.svg.png" class="check"/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Eo_circle_red_letter-x.svg/2048px-Eo_circle_red_letter-x.svg.png" class="check" @click="()=>{
+            DeleteMessage(message.id)
+          }"/>
           
         </div>
       </div>
+    </section>
+
+    <section v-if="state.viewingCompleted === true" class="chat-box-green">
+      <div v-for="message in state.messages" :key="message.key">
+        <div v-if="message.email === state.currentUser.email && message.completed === 'true'" class="taskRow">
+          <div class="task">
+            <p class="content"> {{ message.content }}</p>
+          </div>
+
+        </div>
+      </div>
+
     </section>
 
     <footer>
@@ -49,6 +69,9 @@
 import db from './db'
 import {reactive, onMounted, ref, inject} from 'vue'
 import { decodeCredential, googleLogout } from 'vue3-google-login'
+// import { getStorage, deleteObject } from "firebase/storage";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { update } from '@firebase/database';
 
 export default {
   setup () {
@@ -60,14 +83,17 @@ export default {
     
     const state = reactive({
       currentUser: {
-        email: 'noahdvaughn@gmail.com'
       },
-      messages: []
+      messages: [],
+      viewingCompleted: false
     })
 
     const Logout = () => {
       state.currentUser = null
       // googleLogout()
+    }
+    const ToggleViewing = () => {
+      state.viewingCompleted = !state.viewingCompleted
     }
     
     const SendMessage = () => {
@@ -78,10 +104,26 @@ export default {
       const message = {
         email: state.currentUser.email,
         content: inputMessage.value,
-        completed: false
+        completed: 'false'
       }
       messagesRef.push(message)
       inputMessage.value = ""
+    }
+
+    const DeleteMessage = (key) => {
+      console.log(key)
+      const messagesRef = db.database().ref("messages");
+      messagesRef.child(key).remove()
+    }
+    const markCompleted = (message) => {
+      const messagesRef = db.database().ref("messages");
+      console.log(message)
+      let newMessage = {
+        ...message,
+        completed: 'true'
+      }
+      messagesRef.child(message.id).update(newMessage)
+
     }
 
       onMounted(()=>{
@@ -97,7 +139,8 @@ export default {
           messages.push({
             id: key,
             email: data[key].email,
-            content: data[key].content
+            content: data[key].content,
+            completed: data[key].completed
           })
         })
         state.messages = messages
@@ -108,7 +151,10 @@ export default {
       inputMessage,
       SendMessage,
       Logout,
-      callback
+      callback,
+      DeleteMessage,
+      markCompleted,
+      ToggleViewing
     }
   }
 }
@@ -132,7 +178,23 @@ header{
   height: 10vh;
   display: flex;
   justify-content: center;
-  align-items: center;
+}
+.loginHeader{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 10vh;
+  margin: 0;
+  padding: 0;
+}
+.logout{
+  margin: 0;
+  padding: 0;
+}
+.buttons{
+  display: flex;
+  justify-content: center;
+  padding-top: 1px;
 }
 h1{
   color: white;
@@ -146,7 +208,6 @@ h1{
   align-items: center;
   justify-content: center;
   flex-direction: column;
- overflow-y: scroll;
 
 }
 .login{
@@ -163,9 +224,6 @@ h1{
 .task{
   // border: 2px solid black;
   margin: 10px;
-  // display: flex;
-  // justify-content: space-between;
-  // align-items: center;
 }
 .taskRow{
   display: flex;
@@ -173,12 +231,16 @@ h1{
   align-items: center;
 
 }
-.chat-box{
+.chat-box, .chat-box-green{
   background-color: rgb(255, 198, 41, 0.5);
   border-radius: 20px;
   margin-bottom: 10px;
+  margin-top: 10px;
   width: 70vw;
-  
+ overflow-y: scroll;
+}
+.chat-box-green{
+  background-color: rgb(0, 200, 0, 0.5);
 
 }
 .check{
